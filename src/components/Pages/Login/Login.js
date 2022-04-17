6308544e5c28a357fba0c16f3c8464a983ae7395
siddiq <siddiq.aiub@gmail.com>
2022-04-17 23:30:09 +0600

    complete register

--- src/components/Pages/Login/Login.js
+++ src/components/Pages/Login/Login.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
     return (
-        <div className='container w-25 my-5'>
+        <div className='container w-50 mt-5 p-5 border'>
+            <h3 className='text-center'>Please Login</h3>
             <Form>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" />
                     {/* <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text> */}
@@ -16,7 +19,7 @@ const Login = () => {
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <p>New to Law Academy? <Link to='/register'>Please register.</Link></p>
--- src/components/Pages/Register/Register.js
+++ src/components/Pages/Register/Register.js
@@ -1,14 +1,57 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import  auth  from '../../../firebase.init';
 
 const Register = () => {
+    const nameRef = useRef('');
+    const phoneRef = useRef('');
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    const confirmPasswordRef = useRef('');
+    // const navigate = useNavigate();
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
+    console.log(user)
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        createUserWithEmailAndPassword(email, password);
+    
+    }
+    const handleRegister = () => {
+        // navigate('/login');
+    }
+   
     return (
-        <div className='container w-25 my-5'>
-            <Form>
+        <div className='container w-50 mt-5 border p-5'>
+            <h3 className='mb-b text-center'>Please Register</h3>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="formBasicName">
+                    <Form.Label>Your Name</Form.Label>
+                    <Form.Control ref={nameRef} type="text" name='name' placeholder="Your Name" />
+                    {/* <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text> */}
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPhone">
+                    <Form.Label>Phone</Form.Label>
+                    <Form.Control ref={phoneRef} type="text" name='phone' placeholder="Your Mobile Number" />
+                    {/* <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text> */}
+                </Form.Group>
+               
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control ref={emailRef} type="email" name='email' placeholder="Enter email" />
                     {/* <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text> */}
@@ -16,14 +59,19 @@ const Register = () => {
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control ref={passwordRef} type="password" name='password' placeholder="Password" />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+
+                <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control ref={confirmPasswordRef} type="password" name='confirmPassword' placeholder="Confirm Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="">
                     <p>Already have an account? <Link to='/login'>Please login.</Link></p>
                 </Form.Group>
-                <Button
+                <Button onClick={handleRegister}
                     className='' variant="primary" type="submit">
-                    Login
+                    Register
                 </Button>
             </Form>
         </div>
